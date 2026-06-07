b271d146f65872d087802cb4c0e43eeb4dd7f00d
Angelo Pirola <angelo9871@gmail.com>
2026-06-07 19:37:49 +0200

    Refactor DbContexts: add Source/Target, remove Read/Write
    Removed ReadDbContext and WriteDbContext. Introduced SourceDbContext and TargetDbContext with appropriate DbSet properties for Product and SyncState entities. Updated namespaces and using directives accordingly.

---
+++ src/SyncTwoDatabase/Data/SourceDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncTwoDatabase.Entities;
+
+namespace SyncTwoDatabase.Data;
+
+public class SourceDbContext(DbContextOptions<SourceDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = null!;
+}
---
+++ src/SyncTwoDatabase/Data/TargetDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncTwoDatabase.Entities;
+
+namespace SyncTwoDatabase.Data;
+
+public class TargetDbContext(DbContextOptions<TargetDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<SyncState> SyncStates { get; set; } = null!;
+}
