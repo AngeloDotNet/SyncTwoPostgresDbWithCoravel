f4fa33d60575e8e943f3bca7457845a68326354a
Angelo Pirola <angelo9871@gmail.com>
2026-09-23 09:21:35 +0200

    Add IProductService interface with async product methods
    Introduced IProductService in SyncTwoDatabase.Services.Interfaces, defining async methods GetProductByIdAsync and SaveProductAsync returning Result<Product>. Added necessary using directives for OperationResults and SyncTwoDatabase.Entities.

---
+++ src/SyncTwoDatabase/Services/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using OperationResults;
+using SyncTwoDatabase.Entities;
+
+namespace SyncTwoDatabase.Services.Interfaces;
+
+public interface IProductService
+{
+	Task<Result<Product>> GetProductByIdAsync(Guid id);
+	Task<Result<Product>> SaveProductAsync(Product product);
+}
